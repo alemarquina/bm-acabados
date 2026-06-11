2eb91036b76f1925dfc30120a423d848b2f4cbac
LucianMarquina <alemarquina789@gmail.com>
2026-06-11 12:24:57 -0500

    feat: upgrade SEO website apply sitemap and robots txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-    integrations: [tailwind()],
+    site: 'https://bmacabados.me',
+    integrations: [tailwind(), sitemap()],
     image: {
         service: passthroughImageService(),
     },
